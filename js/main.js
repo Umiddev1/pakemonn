fc201111d32e4bd958e095259e4e62ccac538c6c
Umiddev1 <umidjanrahmanjanov@gmail.com>
2021-12-06 20:43:53 +0500

    end project

---
+++ js/main.js
@@ -0,0 +1,56 @@
+let elPakemonGroups = document.querySelector('.groups__pakemon');
+let elTemplatePakemon = document.querySelector('#pakemon-template').content;
+let elTemplateFr = document.createDocumentFragment();
+let allBtns = document.querySelector('.allbtn');
+let oneBtns = document.querySelector('.onebtn');
+let twoBtns = document.querySelector('.twobtn');
+let arr = [];
+// console.log(elTemplatePakemon);
+for (let pakemon of pokemons.slice(0,100)) {
+  let elPakemon = elTemplatePakemon.cloneNode(true);
+  elPakemon.querySelector('.card__pakemon-imgs').src = pakemon.img;
+  elPakemon.querySelector('.card__pakemon-title').textContent = pakemon.name;
+  elPakemon.querySelector('.card__pakemon-text').textContent =  "#" + pakemon.num;
+  // Fragmentni ichiga solish ma'lumotlarni
+  elTemplateFr.appendChild(elPakemon)
+  arr.push(elPakemon)
+}
+elPakemonGroups.appendChild(elTemplateFr)
+allBtns.addEventListener("click", function(){
+  for (let pakemon of pokemons.slice(0,100)) {
+    elPakemonGroups.innerHTML = ""
+    let elPakemon = elTemplatePakemon.cloneNode(true);
+    elPakemon.querySelector('.card__pakemon-imgs').src = pakemon.img;
+    elPakemon.querySelector('.card__pakemon-title').textContent = pakemon.name;
+    elPakemon.querySelector('.card__pakemon-text').textContent =  "#" + pakemon.num;
+    // Fragmentni ichiga solish ma'lumotlarni
+    elTemplateFr.appendChild(elPakemon)
+  }
+  elPakemonGroups.appendChild(elTemplateFr);
+})
+
+oneBtns.addEventListener("click", function(){
+  for (let pakemon of pokemons.slice(0,50)) {
+    elPakemonGroups.innerHTML = "";
+    let elPakemon = elTemplatePakemon.cloneNode(true);
+    elPakemon.querySelector('.card__pakemon-imgs').src = pakemon.img;
+    elPakemon.querySelector('.card__pakemon-title').textContent = pakemon.name;
+    elPakemon.querySelector('.card__pakemon-text').textContent =  "#" + pakemon.num;
+    // Fragmentni ichiga solish ma'lumotlarni
+    elTemplateFr.appendChild(elPakemon)
+  }
+  elPakemonGroups.appendChild(elTemplateFr)
+})
+
+twoBtns.addEventListener("click", function(){
+  for (let pakemon of pokemons.slice(50,100)) {
+    elPakemonGroups.innerHTML = "";
+    let elPakemon = elTemplatePakemon.cloneNode(true);
+    elPakemon.querySelector('.card__pakemon-imgs').src = pakemon.img;
+    elPakemon.querySelector('.card__pakemon-title').textContent = pakemon.name;
+    elPakemon.querySelector('.card__pakemon-text').textContent =  "#" + pakemon.num;
+    // Fragmentni ichiga solish ma'lumotlarni
+    elTemplateFr.appendChild(elPakemon)
+  }
+  elPakemonGroups.appendChild(elTemplateFr)
+})
